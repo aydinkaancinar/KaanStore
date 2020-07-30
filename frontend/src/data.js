f0a35d30f141152b58c099117f3ea83207edf2d2
Aydin Kaan Cinar <akcinar99@hotmail.com>
2020-07-30 21:06:48 +0300

    added files

---
+++ frontend/src/data.js
@@ -0,0 +1,44 @@
+export default {
+    products: [
+        {
+            _id: '0000001',
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            _id: '0000002',
+            name: 'Fit Shirt',
+            category: 'Shirts',
+            image: '/images/d2.jpg',
+            price: 50,
+            brand: 'Adidas',
+            rating: 4,
+            numReviews: 9
+        },
+        {
+            _id: '0000003',
+            name: 'Regular Pants',
+            category: 'Pants',
+            image: '/images/d3.jpg',
+            price: 70,
+            brand: 'Levi\'s',
+            rating: 5,
+            numReviews: 8
+        },
+        {
+            _id: '0000004',
+            name: 'Slim Pants',
+            category: 'Pants',
+            image: '/images/d4.jpg',
+            price: 65,
+            brand: 'Levi\'s',
+            rating: 4.5,
+            numReviews: 4
+        }
+    ]
+}
